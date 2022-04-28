e55ed3b989425ce535bf53f13c3b3c3c977ed485
pavangajula <pavan.gajula@copart.com>
2022-04-28 15:21:46 +0530

    added styles to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import Coins from "./components/Coins";
+import Coins from "./components/Coins/Coins";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
