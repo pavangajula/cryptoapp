3ba644c17d1433621c75c010405febad6ea10046
pavangajula <pavan.gajula@copart.com>
2022-04-28 13:06:53 +0530

    fetched the coins data using useeffect and usestate from api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React ,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
+import Coins from "./components/Coins";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
+  const url = [
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false",
+  ];
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
-    crypto app
+      <Navbar />
+      <Coins coins={coins} />
     </>
   );
 };
